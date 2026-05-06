9020c50faedae78ec9ee622a26b72063da7bb835
TomkerDev <tomtehassane33@gmail.com>
2026-05-06 17:16:25 +0100

    fix import authModule

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -4,9 +4,11 @@ import { OrderService } from './services/order.service';
 import { OrderValidationService } from './services/order-validation.service';
 import { PrismaService } from '../../prisma.service';
 import { QueueModule } from '../queue/queue.module';
+import { FoodModule } from '../food';
+import { AuthModule } from '../auth';
 
 @Module({
-  imports: [QueueModule],
+  imports: [QueueModule, AuthModule, FoodModule], // ✅ Assure-toi d'importer les modules nécessaires
   controllers: [OrderController],
   providers: [PrismaService, OrderService, OrderValidationService],
   exports: [OrderService],
