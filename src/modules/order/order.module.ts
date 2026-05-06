e34eb8a965e0c69799f3968de5e93dd82d2b49a7
TomkerDev <tomtehassane33@gmail.com>
2026-05-06 12:43:21 +0100

    Ajout des imports dans order module

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { OrderController } from './controllers/order.controller';
+import { OrderService } from './services/order.service';
+import { OrderValidationService } from './services/order-validation.service';
+import { PrismaService } from '../../prisma.service';
+import { QueueModule } from '../queue/queue.module';
 
-@Module({})
+@Module({
+  imports: [QueueModule],
+  controllers: [OrderController],
+  providers: [
+    PrismaService,
+    OrderService,
+    OrderValidationService,
+  ],
+  exports: [OrderService],
+})
 export class OrderModule {}
